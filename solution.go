b3a4c407ae48aa4b02235ed1df43fdabf5b57ab3
Ахметжанов Ильдар <ma9or@mail.ru>
2022-02-16 23:31:02 +0600

    not clear yet

--- solution.go
+++ solution.go
@@ -4,5 +4,5 @@ import (
 	"github.com/kyokomi/emoji/v2" 
 )
 func GetMessage() string {
-	return emoji.Sprint("Hello, world!")  //  this isn't actually a solution:
+	return emoji.Sprint("Hello :world_map:!")  //  this isn't actually a solution:
 }
